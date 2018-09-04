eb389ffbba911005c69bfb419b952bb30a162095
divya.karnakanti <divya.karnakanti@vistex.com>
2018-09-04 11:04:39 +0530

    second commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,40 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast"
-  ], function(Controller,MessageToast) {
-    "use strict";
-  
-    return Controller.extend("sap.ui.demo.walkthrough.controller.App",
-     {
-      onHtmlPress: function() {
-        alert("Hello! Button clicked in HTML View");
-      }, onJsPress: function() {
-        alert("Hello! Button clicked in js View");
-      }, onJsonPress: function(Event) {
-        alert("Hello! Button clicked in json View");
-      },
-      onShowHello : function () {
-        MessageToast.show("Hello World");
-     }
-      
-    }); 
-  });
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast",
+  // "sap/ui/model/json/JSONModel",
+  // "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast) {
+  "use strict";
+
+  return Controller.extend("sap.ui.demo.walkthrough.controller.App",
+    {
+      // onHtmlPress: function () {
+      //   alert("Hello! Button clicked in HTML View");
+      // }, onJsPress: function () {
+      //   alert("Hello! Button clicked in js View");
+      // }, onJsonPress: function (Event) {
+      //   alert("Hello! Button clicked in json View");
+      // },
+      // onShowHello: function () {
+      //   MessageToast.show("Hello World");
+      // },
+      // onInit: function () {
+        // set data model on view
+        // var oData = {
+        //   recipient: {
+        //     name: "World"
+        //   }
+        // };
+        // var oModel = new JSONModel(oData);
+        // this.getView().setModel(oModel);
+        // set i18n model on view
+      //   var i18nModel = new ResourceModel({
+      //     bundleName: "sap.ui.demo.walkthrough.i18n.i18n"
+      //   });
+      //   this.getView().setModel(i18nModel, "i18n");
+      // },
+    
+
+
+    });
+});
